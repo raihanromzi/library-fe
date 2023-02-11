61e24b2b4b08b1df8fbf317deff7f999cc7adb00
Raihan Romzi <raihan.rakhman16@gmail.com>
2023-02-11 13:08:54 +0700

    add submenu for login, loan list

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,19 @@
-import { Box, Button, Flex, HStack, Image, LinkBox, LinkOverlay } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  HStack,
+  Image,
+  LinkBox,
+  LinkOverlay,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList
+} from '@chakra-ui/react';
 import Logo from '../assets/logo-openlibz.png';
 import { useNavigate } from 'react-router-dom';
+import { ChevronDownIcon } from '@chakra-ui/icons';
 
 export default function Navbar() {
   const navigate = useNavigate();
@@ -23,14 +36,34 @@ export default function Navbar() {
             }}>
             Home
           </Button>
-          <Button
-            variant="nav"
-            onClick={() => {
-              navigate('../login');
-            }}>
-            Login
-          </Button>
-          <Button variant="nav">Cart(0)</Button>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              Account
+            </MenuButton>
+            <MenuList>
+              <MenuItem>
+                <Button
+                  variant="nav"
+                  onClick={() => {
+                    navigate('../login');
+                  }}>
+                  Login
+                </Button>
+              </MenuItem>
+              <MenuItem>
+                <Button
+                  variant="nav"
+                  onClick={() => {
+                    navigate('../Home');
+                  }}>
+                  Loan List
+                </Button>
+              </MenuItem>
+              <MenuItem>
+                <Button variant="nav">Cart(0)</Button>
+              </MenuItem>
+            </MenuList>
+          </Menu>
         </HStack>
       </Flex>
     </Box>
